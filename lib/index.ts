10a647ef64da6e5e29c14aaf07c6c09f357ce285
Adrian Cerbaro <adrian@cerbaro.dev>
2024-02-19 09:25:37 -0300

    fix: export only public constants

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ export * from './common/oidc-injection.decorators';
 export * from './common/oidc.decorators';
 export * from './helpers/interaction.helper';
 export * from './interfaces/oidc-module.interface';
-export * from './oidc.constants';
+export { OIDC_PROVIDER, OIDC_PROVIDER_MODULE } from './oidc.constants';
 export * from './oidc.module';
 export * from './oidc.service';
 export * from './types/oidc.types';
